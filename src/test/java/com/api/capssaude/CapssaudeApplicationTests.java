d8d353aec5a7d52370fcbb9ab3e5995c348c1694
João Paulo <costajoaopaulo113@gmail.com>
2024-03-21 13:59:18 -0300

    Subindo backend no repositório

---
+++ src/test/java/com/api/capssaude/CapssaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.capssaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapssaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
