476847cc11aecfa38b2df9cb5c48f430f9cefcdb
João Paulo <costajoaopaulo113@gmail.com>
2024-07-29 20:54:21 -0300

    add: validação real do cpf

--- src/main/java/com/api/capssaude/service/PacienteService.java
+++ src/main/java/com/api/capssaude/service/PacienteService.java
@@ -1,5 +1,8 @@
 package com.api.capssaude.service;
 
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
 import com.api.capssaude.interfaces.IPacienteService;
 import com.api.capssaude.model.Paciente;
 import com.api.capssaude.repository.PacienteRepository;
@@ -18,7 +21,19 @@ public class PacienteService implements IPacienteService {
 
     @Override
     public Paciente save(Paciente paciente) {
+        validateCPF(paciente.getCpf());
         return pacienteRepository.save(paciente);
+    }
+
+    //método para validar o CPF, da dependencia stella core
+    private void validateCPF(String cpf) {
+        CPFValidator cpfValidator = new CPFValidator();
+        try {
+            cpfValidator.assertValid(cpf);
+        } catch (InvalidStateException e) {
+            List<ValidationMessage> messages = e.getInvalidMessages();
+            throw new IllegalArgumentException("CPF Inválido: " + messages.toString());
+        }
     }
 
     @Override
