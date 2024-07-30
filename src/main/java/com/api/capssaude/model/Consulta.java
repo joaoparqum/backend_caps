cd801310dbeefe344c00342d11ec114d454a0440
Everton <everton.araujogta@gmail.com>
2024-07-30 00:22:18 -0300

    Model Consulta

---
+++ src/main/java/com/api/capssaude/model/Consulta.java
@@ -0,0 +1,46 @@
+package com.api.capssaude.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import org.hibernate.validator.constraints.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Entity
+@Table(name = "TB_Consulta")
+@Data
+public class Consulta implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String codigo;
+    @NotNull
+    private String nomePaciente;
+    @NotNull
+    private String cpf;
+    @NotNull
+    private Date data;
+    @NotNull
+    private String horario;
+    @NotNull
+    private String sintomas;
+    private String duracao;
+    @NotNull
+    private List<String> medicamento;
+    @NotNull
+    private String orientacao;
+    private String responsavelLegal;
+}
